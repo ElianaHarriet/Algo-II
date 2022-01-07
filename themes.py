b603880f703a48c2638567a095ac6587807db162
EliHarriet <eharriet@fi.uba.ar>
2022-01-07 13:08:10 -0300

    themes

---
+++ themes.py
@@ -0,0 +1,65 @@
+# ----- PRUEBA DE THEMES ----- #
+for i in range(9):             #
+    while True:                #
+        print("hola")          #
+# ---------------------------- #
+
+
+#dracula 
+#noctis
+#noctis minimus
+#noctis sereno
+#shades of purple
+#solarized dark
+#tomorrow night blue
+
+#cobalt 2
+#noctis obscuro
+#synthwave '84
+
+#winter is coming - dark blue
+
+#tokio hack
+#nighthawk
+#synthwave x fluormachine
+#dark synthwave '84
+
+#synthwave '84 blues
+#neon night
+
+#synthwaves 80's
+ 
+#neon glow theme
+#ultimate dark neo
+#codecourse contrast
+#codecourse
+#fodder
+#gloom
+#horizon
+#kiwi
+#mud
+#penitent contrast
+#penitent
+#piggy
+#prime
+#spearmint contrast
+#stasis
+#vegetable
+
+#lavender contrast
+#lavender
+#warlock contrast
+#warlock
+
+#noctis azureus
+#violaceous contrast
+
+#violaceous
+
+#noctis uva
+
+#midnight synth
+#copilot
+#nightmare
+
+#noctis viola
